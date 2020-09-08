5e486f99234a9c223848fca0596bd4e366b3d061
777tanya <777tanya@list.ru>
2020-09-08 11:56:49 +0300

    сверстали макет

--- idex.php
+++ idex.php
@@ -13,27 +13,56 @@
   <body>
     <div class="container">
       <div class="row">
-      <div class="col-6 coffee-list">
-      <div class="coffee-item">
-        <img src="img/americano.png" alt="Американо">
-        <span>Американо - 50руб</span>
-      </div>
-      <div class="coffee-item">
-        <img src="img/cappuccino.png" alt="Капучино">
-        <span>Капучино - 73руб</span>
-      </div>
-      <div class="coffee-item">
-        <img src="img/espresso.png" alt="Эспрессо">
-        <span>Эспрессо - 61руб</span>
-      </div>
-      <div class="coffee-item">
-        <img src="img/latte.jpg" alt="Латте">
-        <span>Латте - 112руб</span>
-      </div>
-    </div>
+        <div class="col-6 coffee-list d-flex flex-column justify-content-arround">
+          <div class="coffee-item">
+            <img src="img/americano.png" alt="Американо">
+            <span>Американо - 50руб</span>
+          </div>
+          <div class="coffee-item">
+            <img src="img/cappuccino.png" alt="Капучино">
+            <span>Капучино - 73руб</span>
+          </div>
+          <div class="coffee-item">
+            <img src="img/espresso.png" alt="Эспрессо">
+            <span>Эспрессо - 61руб</span>
+          </div>
+          <div class="coffee-item">
+            <img src="img/latte.jpg" alt="Латте">
+            <span>Латте - 112руб</span>
+          </div>
+        </div>
+       <div class="col-6 coffee-oper">
+         <div class="row">
+           <div class="col-6">
+             <div class = "display">
+               <p class = "display-test">Выберите кофе</p>
+               <div class="progress">
+                  <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+                </div>
+             </div>
+             <div class="cup-container">
+               <img src="img/americano.png" alt="" class="cup">
+             </div>
+           </div>
+         <div class="col-6">
+           <div class="input-group mb-3">
+              <input type="text" class="form-control" placeholder="Баланс">
+          <div class="input-group-append">
+            <span class="input-group-text" id="basic-addon2">&#8381</span>
+         </div>
+        </div>
+        <div class="atm-container">
+          <img src="img/bill_acc.png" alt="" class="atm">
+        </div>
+     <button class="btn btn-primary btn-block my-2">Сдача</button>
+     <div class="change-container">
+       
+     </div>
+     </div>
   </div>
- </div>
-    <!-- Optional JavaScript -->
+  </div>
+   </div>
+   <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
